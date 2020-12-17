31e04e7964faa37cb622e08b9e73b011b9c0ea26
hasan <hasan.maximmo@gmail.com>
2020-12-17 21:57:41 +0100

    api commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserList from "./component/UserList";
 function App() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  const getUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(function (response) {
+        setUsers(response.data);
+      })
+      .catch(function (error) {
+        setError(error);
+      });
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  console.log(error);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">User's List</div>
+      <div className="user-container">
+        <UserList data={users} />
+      </div>
     </div>
   );
 }
